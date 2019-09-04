37a735374aa32a90bae7a64afd2512c70a29c993
Maribal <maribal812@gmail.com>
2019-09-04 19:03:03 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import React, {Component} from 'react';
+import './bootstrap.min.css';
+import Header from './components/Header';
+import NuevaCita  from './components/NuevaCita';
+import ListaCitas  from './components/ListaCitas';
+
+class App extends Component{
+
+  state={
+      citas:[]
+  }
+
+  crearNuevaCita = datos => {
+    const citas = [...this.state.citas, datos];
+
+    this.setState({
+      citas
+    });
+  }
+
+  render(){
+    return(
+      <div className="container">
+        <Header title='Administrador Pacientes Veterinaria' />
+
+        <div className="row">
+            <div className="col-md-10 mx-auto">
+                <NuevaCita
+                  crearNuevaCita={this.crearNuevaCita}
+                 />
+            </div>
+            <div className="mt-5 col-md-10 mx-auto">
+                <ListaCitas citas={this.state.citas}/>
+            </div>
+        </div>
+      </div>
+     
+    );
+  }
+}
+
+export default App;
